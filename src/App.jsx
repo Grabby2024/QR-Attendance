5b38ba20763eff6c1fcfefbfa8089dcf804ba29d
Grabby2024 <leornas.g20@gmail.com>
2025-09-30 13:48:14 +0800

    Fix case-sensitive import for ListofAttendance

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AdminDashboard from "./pages/AdminDashboard";
 import AttendanceScanner from "./pages/AttendanceScanner";
 
-//Aadmin Components
+// Admin Components
 import Home from "./pages/components/Home";
 import Offices from "./pages/components/Offices";
 import Statistical_Data from "./pages/components/Statistical_Data";
-import ListOfAttendance from "./pages/components/ListOfAttendance";
+import ListofAttendance from "./pages/components/ListofAttendance"; // ✅ match file name exactly
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route index element={<Home />} /> {/* /admin */}
           <Route path="offices" element={<Offices />} /> {/* /admin/offices */}
           <Route path="statistics" element={<Statistical_Data />} /> {/* /admin/statistics */}
-          <Route path="attendance-list" element={<ListOfAttendance />} /> {/* /admin/attendance-list */}
+          <Route path="attendance-list" element={<ListofAttendance />} /> {/* /admin/attendance-list */}
         </Route>
 
         {/* Scanner route (not part of admin) */}
